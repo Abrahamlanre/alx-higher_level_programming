0b66aadd77b0aed83cefb3fc22ee3acfab4e178e
Abrahamlanre <lanreabraham44@gmail.com>
2022-12-21 03:28:05 +0000

    0x06-python-classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,8 +2,10 @@
 """Square module definition.
 This module defines a simple `Square` class
 """
+
+
 class Square:
-"""The simplest ``Square`` class
-For now the class has neither ``Atribures`` nor ``Methods``
-"""
-pass
+    """The simplest ``Square`` class
+    For now the class has neither ``Atribures`` nor ``Methods``
+    """
+    pass
